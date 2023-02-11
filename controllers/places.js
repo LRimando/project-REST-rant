b94b6eabdbf8166704ea3a0efd6692b46140944a
Laila Rimando <lrimando7@gmail.com>
2023-02-10 19:28:12 -0800

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-router.get ('/:id', (req,res) => {
+router.put('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
         res.render('error404')
@@ -10,22 +10,23 @@ router.get ('/:id', (req,res) => {
         res.render('error404')
     }
     else {
-        places.splice(id, 1)
-        res.render('places/edit', {place: places[id] })
+        // Dig into req.body and make sure data is valid
+        if (!req.body.pic) {
+            // Default image if one is not provided
+            req.body.pic = 'http://placekitten.com/400/400'
+        }
+        if (!req.body.city) {
+            req.body.city = 'Anytown'
+        }
+        if (!req.body.state) {
+            req.body.state = 'USA'
+        }
+  
+        // Save the new data into places[id]
+        places[id] = req.body
+        res.redirect(`/places/${id}`)
     }
-    console.log(req.body)
-    if (!req.body.pic) {
-        // Default image if one is not provided
-        req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-        req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-        req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.render('places/show')
-})
+  })
+  
 
 module.exports = router 
