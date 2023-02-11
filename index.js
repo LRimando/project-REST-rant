b4958bcf0c3423145d690d53006d6d5adb5caf65
Laila Rimando <lrimando7@gmail.com>
2023-02-10 17:48:52 -0800

    Adding remaining stub routes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-const express = require('express')
+require ('dotenv').config()
+const express = require ('express')
 const app = express()
 
-app.use('/places', require('./controllers/places'))
+app.use('/places', require ('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send ('Hello world!')
+    res.send('Hello world!')
 })
 
 app.get('*', (req,res) => {
